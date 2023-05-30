df0e29fcb34752df677a9233f70c001b034ca61f
Musasi <musasi.art@gmail.com>
2023-05-30 10:46:05 +0800

    update files

---
+++ js/script.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+
+  document.getElementsByTagName('input')[0].addEventListener('keypress', function(event){
+    if(event.key == 'Enter'){
+      document.getElementsByClassName('loading')[0].style.opacity = 1;
+      setTimeout(function(){
+        window.location.href = 'result.html';
+      }, 7000);
+    }
+  })
+
+  document.getElementsByClassName('bi-send')[0].addEventListener('click', function(){
+    document.getElementsByClassName('loading')[0].style.opacity = 1;
+    setTimeout(function(){
+      window.location.href = 'result.html';
+    }, 7000);
+  })
+
+
+}
+
+if(window.innerWidth <= 428){
+  $('.playing').children().eq(11).nextAll().css('display', 'none');
+}
